c3512285b7a2d4eea9946f9a1587a91574547c44
Isabelle <isabelle.tieu@gmail.com>
2021-11-22 15:33:32 +0100

    webpack setup done

---
+++ projetTS/webpack.config.prod.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	entry: "./src/index.ts",
+	output: {
+		filename: "bundle.[contenthash].js", //contenthash est optionnel
+		clean: true,
+		path: path.resolve(__dirname, "dist"), //chemin absolu
+	},
+	mode: "production",
+	module: {
+		rules: [
+			//Configuration compilation TS
+			{
+				test: /\.ts$/,
+				use: "ts-loader",
+				exclude: /node_modules/,
+			},
+		],
+		plugins: [
+			new HtmlWebpackPlugin({
+				template: "src/index.html",
+			}),
+		],
+	},
+	resolve: {
+		extensions: [".ts", ".js"],
+	},
+};

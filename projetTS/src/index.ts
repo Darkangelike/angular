55507bcf71c4e09152547310b21d9946515039d2
Isabelle <isabelle.tieu@gmail.com>
2021-11-23 10:47:13 +0100

    video games list - done object list

--- projetTS/src/index.ts
+++ projetTS/src/index.ts
@@ -3,13 +3,13 @@ import "./css/base.css";
 import "./css/style.css";
 
 enum PEGIlist {
-	everyone = "Everyone",
-	class3 = "3 years old",
-	class7 = "7 years old",
-	class12 = "12 years old",
-	class16 = "16 years old",
-	class18 = "18 years old",
-	badLanguage = "Bad language",
+	e = "Everyone",
+	c3 = "3 years old",
+	c7 = "7 years old",
+	c12 = "12 years old",
+	c16 = "16 years old",
+	c18 = "18 years old",
+	language = "Bad language",
 	discrimination = "Discrimination",
 	drugs = "Drugs",
 	fear = "Fear",
@@ -36,14 +36,52 @@ let zelda: Game = {
 	name: "Ocarina of Time",
 	genre: ["Adventure,"],
 	publisher: "Nintento",
-	PEGI: [PEGIlist.everyone, PEGIlist.class12],
+	PEGI: [PEGIlist.e, PEGIlist.c12],
 	year: 1998,
 	multiplayer: false,
 	online: false,
 	image: "images/The_Legend_of_Zelda_Ocarina_of_Time.jpg",
 };
 
+let phasmophobia: Game = {
+	name: "Phasmophobia",
+	genre: ["Horror"],
+	publisher: "Kinetic Games",
+	PEGI: [PEGIlist.fear, PEGIlist.c18],
+	year: 2020,
+	multiplayer: true,
+	online: true,
+	image: "./images/jeu-steam-phasmophobia-early-access-cover.jpg",
+};
+
+let bustAMove: Game = {
+	name: "Bust-a-Move",
+	genre: ["Arcade"],
+	publisher: "Taito",
+	PEGI: [PEGIlist.e],
+	year: 1994,
+	multiplayer: true,
+	online: false,
+	image: "./images/index.jpg",
+};
+
+let tetris: Game = {
+	name: "tetris",
+	genre: ["puzzle", "arcade"],
+	publisher: "Electronic Arts",
+	PEGI: [PEGIlist.e],
+	year: 1984,
+	multiplayer: true,
+	online: true,
+	image: "./images/Tetris_NES_cover_art.jpg",
+};
+
 gamesList.push(zelda);
 
-const content: HTMLElement = document.getElementById("content") as HTMLElement;
-content.innerHTML = "Hello World";
+function displayOneItem(list: Object[], targetContainerID: string) {
+	let content: HTMLElement = document.getElementById(
+		targetContainerID
+	) as HTMLElement;
+
+	// content.innerHTML =
+}

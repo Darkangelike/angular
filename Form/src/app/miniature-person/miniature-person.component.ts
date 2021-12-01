3f14b894b98546f3159164e2a22168966e4d6ed3
Isabelle <isabelle.tieu@gmail.com>
2021-12-01 14:12:36 +0100

    Form - miniature person initialised

---
+++ Form/src/app/miniature-person/miniature-person.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-miniature-person',
+  templateUrl: './miniature-person.component.html',
+  styleUrls: ['./miniature-person.component.css']
+})
+export class MiniaturePersonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
